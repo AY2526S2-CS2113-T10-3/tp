571a8d8339eff68150a691f2422851602bf9ee31
Karthik <karthi12kathir@gmail.com>
2026-04-01 23:55:40 +0800

    Add assertions and logging to improve the defensiveness of the code

--- src/main/java/seedu/pharmatracker/command/DeleteCustomerCommand.java
+++ src/main/java/seedu/pharmatracker/command/DeleteCustomerCommand.java
@@ -1,5 +1,8 @@
 package seedu.pharmatracker.command;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import seedu.pharmatracker.customer.Customer;
 import seedu.pharmatracker.customer.CustomerList;
 import seedu.pharmatracker.data.Inventory;
@@ -13,6 +16,7 @@ public class DeleteCustomerCommand extends Command {
 
     public static final String COMMAND_WORD = "delete-customer";
 
+    private static final Logger logger = Logger.getLogger(DeleteCustomerCommand.class.getName());
     private final String description;
 
     public DeleteCustomerCommand(String description) {
@@ -21,11 +25,20 @@ public class DeleteCustomerCommand extends Command {
 
     @Override
     public void execute(Inventory inventory, Ui ui, CustomerList customerList) {
+        logger.log(Level.INFO, "Starting execution of DeleteCommand for index: " + description);
+
+        assert customerList != null : "Customer list cannot be empty in DeleteCustomerCommand";
+        assert ui != null : "Ui cannot be null in DeleteCustomerCommand";
+
         int index = Integer.parseInt(description);
         int zeroBasedIndex = index - 1;
 
         Customer customer = customerList.getCustomer(zeroBasedIndex);
+        assert customer != null : "Retrieved customer to be removed cannot be null";
+
         customerList.removeCustomer(customer);
         ui.printDeletedCustomerMessage(customer, customerList);
+
+        logger.log(Level.INFO, "Successfully executed DeleteCommand");
     }
 }
